8e9072147c16c68e5976381e1270876649c1dc90
prathamesh22davande <prathamesh22davande@gmail.com>
2021-05-10 21:12:29 +0530

    Select farm option added

--- models/Task.js
+++ models/Task.js
@@ -4,6 +4,7 @@ const schema = mongoose.Schema;
 
 const taskSchema = new schema({
   uid: { type: String, required: true },
+  farmId: { type: String, required: true },
   taskName: { type: String, required: true },
   taskDescription: { type: String, required: true },
   date: { type: Date, default: Date.now },
